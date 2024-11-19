3017627e8e00544c3cdd037fb1f0aae22e17efe4
medintelx <137313960+medintelx@users.noreply.github.com>
2024-11-19 20:28:42 +0530

    Update ndot-ui-latest-devops.py

--- ndot-ui-latest-devops.py
+++ ndot-ui-latest-devops.py
@@ -869,6 +869,31 @@ def main_application():
             'projects_Official_DOC_Date': lambda x: pd.to_datetime(x).strftime('%m/%d/%Y') if pd.notnull(x) else ''
       
         }
+        format_non_anchor_dict = {
+    'total_effort_from_pbis': '{}',
+    'nearest_doc_date': lambda x: x.strftime('%m/%d/%Y') if pd.notnull(x) else '',
+    'projects_Scoping_30_Percent': lambda x: x.strftime('%m/%d/%Y') if pd.notnull(x) else '',
+    'projects_SeventyFivePercentComplete': lambda x: x.strftime('%m/%d/%Y') if pd.notnull(x) else '',
+    'projects_Intermediate_Date': lambda x: x.strftime('%m/%d/%Y') if pd.notnull(x) else '',
+    'projects_QAQC_Submittal_Date': lambda x: x.strftime('%m/%d/%Y') if pd.notnull(x) else '',
+    'projects_Document_Submittal_Date': lambda x: x.strftime('%m/%d/%Y') if pd.notnull(x) else '',
+    'projects_Official_DOC_Date': lambda x: x.strftime('%m/%d/%Y') if pd.notnull(x) else '',
+    'projects_Fiscal_Year': '{:.0f}',
+    'projects_Priority_Traffic_Ops': '{:.0f}',
+    'projects_Construction_EA_Number': '{:.0f}',
+    'projects_Work_Item_ID': '{:.0f}',
+    'epics_System_Id': '{:.0f}',
+    'projects_Anchor_Project': '{:.0f}',
+    'projects_Complexity_Signals': '{:.0f}',
+    'projects_Complexity_Lighting': '{:.0f}',
+    'projects_Complexity_ITS': '{:.0f}',
+    'projects_Complexity_Power_Design': '{:.0f}',
+    'projects_Complexity_RoW_Coordination': '{:.0f}',
+    'projects_Complexity_SLI_Project_Lead': '{:.0f}',
+    'projects_Complexity_Solar_Design': '{:.0f}',
+    'projects_Complexity_Trunkline': '{:.0f}',
+    'projects_complexity': '{:.0f}'
+}
         
         # Apply styling to format columns
         styled_anchor_df = anchor_projects_df.style.format(format_dict)
@@ -893,7 +918,7 @@ def main_application():
         
 
             # Apply styling to format columns
-        styled_non_anchor_df = non_anchor_project_df.style.format(format_dict)
+        styled_non_anchor_df = non_anchor_project_df.style.format(format_non_anchor_dict)
         st.dataframe(styled_non_anchor_df, hide_index=True)
         st.write("Data from Azure devops")
         # st.button("Refresh")
